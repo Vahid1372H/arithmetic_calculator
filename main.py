8d1b8a831634a650b8d2abd84aed644dcb689ffb
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-04 21:15:25 +0330

    define validation in get_float_number function

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from iostream import get_float_number
 from operations import get_operator
 
 
+
 def get_float_number():
-    try:
-        user1 = input("Enter your float_number1: ")
-        user2 = input("Enter your float_number2: ")
-        float(user1) or float(user2)
-    except:
-        raise ValueError ("The number type should be 'float'.")
+    user1 = input("Enter your float_number1: ")
+    user2 = input("Enter your float_number2: ")
+    if user1.isnumeric() or user2.isnumeric():
+        return False 
+    return True
 
 
 
