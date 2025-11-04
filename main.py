aa9cd82ddcac5e59fdb48a73475dd03a26401737
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-04 20:26:01 +0330

    import get_float_number function and get_operator function and define get_float_number function

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
+from iostream import get_float_number
+from operations import get_operator
+
+
+def get_float_number():
+    user1 = input("Enter your number: ")
+    user2 = input("Enter your number: ")
+    if not user1.isnumeric() or user2.isnumeric():
+        return True
+    return False
+    
+def addition():
+    pass  
+
+
+def subtraction():
+    pass
+
+
+def multiplication():
+    pass
+
+
+def division():
+    pass
+
+
+def division_floor():
+    pass
+
+
+def modulus():
+    pass
+
+
+def power():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
     pass
 
