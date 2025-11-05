5c275a6d2d99e2ec487ca60e2a8d7cb44e17fdd5
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-05 19:48:08 +0330

    test function is changed

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ def power():
 
 
 def main():
-    number_1 = input("Enter your user1")
-    number_2 = input("Enter your user2")
+    number_1 = input("Enter your user1: ")
+    number_2 = input("Enter your user2: ")
     s = number_1 + number_2
     print(s)
 print(get_float_number())
