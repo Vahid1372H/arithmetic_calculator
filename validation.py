5ba0cb0ab8692d411833fec6ce306f5b7278a737
Vahid Hajikhani <vahid_hajikhani@yahoo.com>
2025-11-14 14:06:37 +0330

    if is deleted

--- validation.py
+++ validation.py
@@ -14,12 +14,6 @@ def is_float(number: str) -> bool:
         return False
 
 
-if __name__ == "__main__":
-    print("test of is_float: ")
-    print(f"True mode (number = 12.5): {is_float("12.5")}")
-    print(f"False mode (number = 12q5): {is_float("12q5")}")
-
-
 def is_operator(operator: str) -> bool:
     """validation of is_operator function
 
@@ -29,13 +23,16 @@ def is_operator(operator: str) -> bool:
     Returns:
         bool: output of validation is True or False
     """
-    operators = {"-", "+", "*", "/", "//", "**","%"}
-    if not operator in operators :
+    operators = {"-", "+", "*", "/", "//", "**", "%"}
+    if not operator in operators:
         return False
     return True
 
 
 if __name__ == "__main__":
+    print("test of is_float: ")
+    print(f"True mode (number = 12.5): {is_float("12.5")}")
+    print(f"False mode (number = 12q5): {is_float("12q5")}")
     print("test of is_operator: ")
     print(f"True mode (operator = *): {is_operator("*")}")
     print(f"False mode (operator = 1@): {is_operator("1@")}")
